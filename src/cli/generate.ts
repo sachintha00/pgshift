b94edc0a034170fd00d09944d4339aab427cf664
sachintha0603 <sachintha0603@gmail.com>
2025-04-18 11:19:58 +0530

    first release

--- src/cli/generate.ts
+++ src/cli/generate.ts
@@ -2,6 +2,7 @@
 import { Generator } from "../lib/generator";
 import * as path from "path";
 
+// No environment loading needed for generate commands
 async function run() {
   const command = process.argv[2]; // 'generate' or 'generate-seeder'
   const name = process.argv[3]; // e.g., 'create_users_table' or 'seed_users'
